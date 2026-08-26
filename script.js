b41af5379b20116489f5d4870712eb26892089dd
Vitória Manuella <freitas.melo.vitoria@escola.pr.gov.br>
2026-08-26 19:39:34 -0300

    desenvolvimento do js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const botao = document.querySelector()
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
+
+function botaoClicado() {
+    console.log("fui clicado");
+}
