2d04558e5e17ce7da3164a7267ec81da4bbfda61
Vitória Manuella <freitas.melo.vitoria@escola.pr.gov.br>
2026-08-26 20:44:31 -0300

    js alteração

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const botao = document.querySelector("button");
-botao.addEventListener("click", botaoClicado);
+const botoes = document.querySelectorAll("button");
 
-function botaoClicado(){
-    console.log("fui clicado");
-    let texto = botao.querySelector("span");
-    texto.textContent++;
-}
+botoes.forEach( function(botao){
+    botao.addEventListener("click", botaoClicado);
+    function botaoClicado(){
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+    }
+} )
+
