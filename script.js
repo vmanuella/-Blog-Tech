f50a871542b9482c173234c3a83f801147b627b6
Vitória Manuella <freitas.melo.vitoria@escola.pr.gov.br>
2026-08-26 21:23:53 -0300

    js completo

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const botoes = document.querySelectorAll("button");
 
 botoes.forEach( function(botao){
+    let curtiu = false;
     botao.addEventListener("click", botaoClicado);
     function botaoClicado(){
         console.log("fui clicado");
         let texto = botao.querySelector("span");
-        texto.textContent++;
+        if (curtiu === false){
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu - false;
+        }
     }
-} )
+})
 
