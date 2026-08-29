7a193c6a606647dd151846948da9ba6e9394481f
Vitória Manuella <freitas.melo.vitoria@escola.pr.gov.br>
2026-08-29 16:24:44 -0300

    interação ao botão

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ botoes.forEach( function(botao){
     }
 })
 
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+btnTemaEscuro.addEventListener("click", mudaTema);
+
+function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
